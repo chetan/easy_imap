d077a87c29ec7b3557ed96111e0405b7120bf168
Chetan Sarva <chetan@pixelcop.net>
2011-02-02 00:07:01 -0500

    implemented message deletion

--- lib/easy_imap/folder.rb
+++ lib/easy_imap/folder.rb
@@ -1,6 +1,10 @@
 module EasyIMAP
   # A Folder on the IMAP server.
   class Folder
+
+    READ_ONLY   = 0
+    READ_WRITE  = 1
+
     # the name of the folder.
     attr_reader :name
 
@@ -17,7 +21,7 @@ module EasyIMAP
 
     # An array of messages in this folder.
     def messages
-      @conn.examine(@full_name)
+      read_only()
       @conn.uid_search(['ALL']).map do |uid|
         Message.new(@conn, uid)
       end
@@ -28,6 +32,25 @@ module EasyIMAP
       @conn.list("#{@full_name}#{@delim}", '%').map do |f|
         Folder.new(@conn, f.name, f.delim)
       end
+    end
+
+    # Delete the given message or uid
+    def delete(msg)
+      read_write()
+      uid = (msg.kind_of?(EasyIMAP::Message) ? msg.uid : msg)
+      @conn.uid_store(uid, '+FLAGS', [:Deleted])
+    end
+
+    def read_only
+      return if @mode == READ_ONLY
+      @mode = READ_ONLY
+      @conn.examine(@full_name)
+    end
+
+    def read_write
+      return if @mode == READ_WRITE
+      @mode = READ_WRITE
+      @conn.select(@full_name)
     end
   end
 end
